006467cbcba4a0dc75518328644e9380e471e011
Stephan Koster <skoster@enclustra.com>
2025-08-22 13:57:47 +0200

    started basic cli with click library

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import click
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@click.command()
+@click.option('--number', default=1, help='number to print')
+def test(number):
+    print(f'this is only a test no cause for alarm. Your number is {number}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    test()
